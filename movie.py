1db2fe51cfe5cc1a736275405ad8ddc7dd25b303
Laura Tatiana Munoz <lauramunga@unisabana.edu.co>
2026-05-02 16:51:13 -0500

    v8

--- movie.py
+++ movie.py
@@ -1,23 +1,17 @@
-# Write your code here!
-# FREEZE CODE BEGIN
 class Movie:
     def __init__(self, title, director, year):
         self.title = title
         self.director = director
         self.year = year
-# FREEZE CODE END
+
     def __str__(self):
         return f"Movie: {self.title} (Directed by {self.director}, {self.year})"
 
 
-# FREEZE CODE BEGIN
 if __name__ == "__main__":
-    # --- Main Program ---
-    title = input("Enter title:\n ")
-    director = input("Enter director:\n ")
-    year = int(input("Enter year:\n "))
-# FREEZE CODE END
+    title = input("Enter title:\n")
+    director = input("Enter director:\n")
+    year = int(input("Enter year:\n"))
     
-    
-my_movie = Movie(title, director, year)
-print(my_movie)
+    my_movie = Movie(title, director, year)
+    print(my_movie)
