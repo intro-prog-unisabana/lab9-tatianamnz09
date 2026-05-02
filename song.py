3513f047b11bdad4f9584bfd472de9c3b99a9d1d
Laura Tatiana Munoz <lauramunga@unisabana.edu.co>
2026-05-01 22:23:54 -0500

    v3

--- song.py
+++ song.py
@@ -7,5 +7,5 @@ class Song:
     def get_length_in_seconds(self):
         return self.length * 60
 
-my_song = Song("tv off", "Kendrick Lamar", 3.7)
-print(my_song.get_length_in_seconds())  
+def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
